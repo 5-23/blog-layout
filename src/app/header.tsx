35d0a80fc657a66bd5cc0f910991532dc20a3da1
5-23 <yhanbyeol6@gmail.com>
2023-06-30 21:42:16 +0900

    make system

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,7 +3,11 @@ export default function Header(){
     return(
         <header>
             <input type="checkbox" id="open-list"/>
-            <nav></nav>
+            <nav>
+                <a href="#1">1. 대충</a>
+                <a href="#2">2. 엄청난</a>
+                <a href="#1">3. 내용</a>
+            </nav>
 
             <label htmlFor="open-list">
                 <p></p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <main style={{paddingTop: "100px"}}>
+      <main style={{padding: "100px"}}>
+        <h1>1. 제목</h1> <hr />
+        <h2>엄청난 내용</h2>
       </main>
     </>
   )
